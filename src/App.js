9f16018bacf1244fa1c2e918a6584b3cc9c92f47
zcason <zuri.cason@gmail.com>
2020-10-05 14:14:21 -0400

    wq wq~ quit :wq :wq q

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Google Books Search</h1>
-    </div>
-  );
-}
 
+const apiKey = 'AIzaSyDVWGglThk31c86Ty0NCsZgSLTrA0HsTGs';
+
+class App extends React.Component {
+
+  state = {
+    books: [],
+    loading: false,
+    error: null
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true })
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=quilting&key=${apiKey}`)
+      .then(response => response.ok ? response.json() : Promise.reject('Something went wrong'))
+      .then(dataJson => dataJson.items)
+      .then(items => this.setState({
+        books: items,
+        loading: false
+      }))
+      .catch(error => this.setState({ error, loading: false }))
+
+
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div></div>
+    );
+  }
+
+}
 export default App;
